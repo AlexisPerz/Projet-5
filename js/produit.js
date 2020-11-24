cd8670367ce4ce9d6048ebc0e21f351ef87e005b
Alexis <perz.alexis.sio@gmail.com>
2020-11-24 10:26:40 +0100

    test panier

--- js/produit.js
+++ js/produit.js
@@ -1,20 +1,20 @@
 const queryString = window.location.search;
-const id = new URLSearchParams(queryString).get('id');
-let main = document.getElementById("main");
 
+const id = new URLSearchParams(queryString).get('id');
+
+let main = document.getElementById("main");
 
 fetch("http://localhost:3000/api/teddies/"+id)
 .then((res) =>{
 	res.json().then(product=>{
 		//On créer une div et on lui attribut une classe
 		let div=document.createElement('DIV');
-		div.classList.add("col-lg-4 col-md-6 col-sm-12");
+		div.classList.add("col-4");
 
 		//On créer une balise a et on lui attribut un href et on récupère le nom du produit
 		let a=document.createElement('A');
 		a.href="produit.html?id="+product._id;
 		a.innerHTML=product.name+'<br/>';
-		
 
 		//On créer une balise img et on récupère l'Url de l'image
 		let img=document.createElement('img');
@@ -34,3 +34,70 @@ fetch("http://localhost:3000/api/teddies/"+id)
 
 	})
 })
+
+let carts = document.querySelectorAll(".panier");
+
+let products = [
+	{
+		name: 'Norbert',
+		tag: 'Ours 1',
+		price: 2900,
+		inCart: 0
+	},
+	{
+		name: 'Arnold',
+		tag: 'Ours 2',
+		price: 3900,
+		inCart: 0
+	},
+	{
+		name: 'Lenny and Carl',
+		tag: 'Ours 3',
+		price: 5900,
+		inCart: 0
+	},
+	{
+		name: 'Gustav',
+		tag: 'Ours 4',
+		price: 4500,
+		inCart: 0
+	},
+	{
+		name: 'Garfunkel',
+		tag: 'Ours 5',
+		price: 5500,
+		inCart: 0
+	}
+];
+
+for (var i = 0; i < carts.length; i++) {
+	carts[i].addEventListener("click", () => {
+		cartNumbers(products[i]);
+	})
+}
+
+function onLoadCartNumbers() {
+	let productNumbers = localStorage.getItem('cartNumbers');
+
+	if (productNumbers) {
+		document.querySelector('.nav-item span').textContent = productNumbers;
+	}
+}
+
+function cartNumbers(product) {
+	console.log("Le produit sélectionner est ", product);
+
+	let productNumbers = localStorage.getItem('cartNumbers');
+
+	productNumbers = parseInt(productNumbers);
+
+	if (productNumbers) {
+		localStorage.setItem('cartNumbers', productNumbers + 1);
+		document.querySelector('.nav-item span').textContent = productNumbers + 1;
+	} else {
+		localStorage.setItem('cartNumbers', 1);
+		document.querySelector('.nav-item span').textContent = 1;
+	}
+}
+
+onLoadCartNumbers();
