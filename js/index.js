b418715d11f5c72c612b36d98e50dd501b780552
Alexis <perz.alexis.sio@gmail.com>
2020-11-27 12:24:43 +0100

    testimage

--- js/index.js
+++ js/index.js
@@ -13,23 +13,22 @@ fetch("http://localhost:3000/api/teddies")
 		let div=document.createElement('DIV');
 		div.classList.add("col-4");
 
-		//On créer une balise a et on lui attribut un href et on récupère le nom du produit
-		let a=document.createElement('A');
-		a.href="produit.html?id="+product._id;
-		a.innerHTML=product.name+'<br/>';
-		
-
 		//On créer une balise img et on récupère l'Url de l'image
 		let img=document.createElement('img');
 		img.src=product.imageUrl;
 
+		//On créer une balise a et on lui attribut un href et on récupère le nom du produit
+		let a=document.createElement('A');
+		a.href="produit.html?id="+product._id;
+		a.innerHTML=product.name+'<br/>';
+
 		//On créer une balise p et on récupère le prix 
 		let p=document.createElement('P');
-		p.innerHTML='Prix : '+product.price+'€';
+		p.innerHTML=product.price+'€';
 
 		//On relis nos éléments à la div
-		div.appendChild(a);
 		div.appendChild(img);
+		div.appendChild(a);
 		div.appendChild(p);
 
 		//On relis notre div à la balise main
--- js/produit.js
+++ js/produit.js
@@ -12,31 +12,52 @@ fetch("http://localhost:3000/api/teddies/"+id)
 
 		displayColor(product);
 
+		// Écoute de notre formulaire
+		const form = document.getElementById("form");
+		form.addEventListener('submit', function (e) {
+		e.preventDefault();
+		const selectColors = document.getElementById('color');
+		const teddyId = product._id;
+		const color = selectColors.value;
+		localStorage.setItem("cart", JSON.stringify({color, teddyId}));
+		console.log(teddyId, color);
+		})
 	})
 })
 
 function displayProduct(product){
+
+		const section = document.querySelector('section');
+
 		//On créer une div et on lui attribut une classe
 		let div=document.createElement('DIV');
-		div.classList.add("col-4");
-
-		//On créer une balise a et on lui attribut un href et on récupère le nom du produit
-		let a=document.createElement('A');
-		a.href="produit.html?id="+product._id;
-		a.innerHTML=product.name+'<br/>';
+		div.classList.add("carte");
 
 		//On créer une balise img et on récupère l'Url de l'image
 		let img=document.createElement('img');
 		img.src=product.imageUrl;
+		img.classList.add("imageproduit");
+
+		//On créer une balise p et on récupère le nom du produit
+		let p=document.createElement('P');
+		p.innerHTML=product.name;
+		p.classList.add("nomproduit");
 
 		//On créer une balise p et on récupère le prix 
-		let p=document.createElement('P');
-		p.innerHTML='Prix : '+product.price+'€';
+		let p2=document.createElement('P');
+		p2.innerHTML=product.price+'€';
+		p2.classList.add("prixproduit");
+
+		//On créer une balise p et on récupère la description du produit
+		let p3=document.createElement('P');
+		p3.innerHTML=product.description;
+		p3.classList.add("descriptionproduit");
 
 		//On relis nos éléments à la div
-		div.appendChild(a);
-		div.appendChild(img);
+		section.appendChild(img);
 		div.appendChild(p);
+		div.appendChild(p2);
+		div.appendChild(p3);
 
 		//On relis notre div à la balise main
 		main.appendChild(div);
